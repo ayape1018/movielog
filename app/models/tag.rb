0e6adf310aa32c355c4fb138cf0bad7e3b4ab6be
ayape1018 <ayape1018ca@gmail.com>
2020-05-26 17:38:26 -0400

    fix WorkTag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-  has_many: work_tags, dependent: :destroy
-  has_many: works, through: :work_tags
+  has_many :work_tags, dependent: :destroy
+  has_many :works, through: :work_tags
 end
